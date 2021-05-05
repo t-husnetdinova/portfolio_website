98dd40538c1873d51998f6ee5bbf56fec262c8e9
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-05-05 15:10:18 -0600

    reworked social media icons

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,8 +5,28 @@ import './HeroSection.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <h1>Welcome to my site!</h1>
+            <h1>Welcome!</h1>
             <p>Make yourself at home :)</p>
+            <div>
+            <ul class="socials" id="socials">
+                <li class="socials-item">
+                    <a href="https://github.com/t-husnetdinova" >
+                        <i class="fab fa-2x fa-github"></i>
+                    </a>
+                </li>
+                <li class="socials-item">
+                    <a href="https://www.linkedin.com/in/tammy-husnetdinova-25150b171/">
+                        <i class="fab fa-2x fa-linkedin"></i>
+                    </a>
+                </li>
+                <li class="socials-item">
+                    <a href="https://www.youtube.com/channel/UCwiQYSftmZ2joZsotbUarTg">
+                        <i class="fab fa-2x fa-youtube"></i>
+                    </a>
+                </li>
+                </ul>
+            </div>
+
         </div>
     )
 }
