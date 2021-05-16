b92bddd8fd13267307a141f2689ce4242ac521d1
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-05-16 16:46:01 -0600

    socials and background fixed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,23 +6,6 @@ function HeroSection() {
     return (
         <div className='hero-container'>
             
-            <ul class="socials" id="socials">
-                <li class="socials-item">
-                    <a href="https://github.com/t-husnetdinova" >
-                        <i class="fab fa-2x fa-github"></i>
-                    </a>
-                </li>
-                <li class="socials-item">
-                    <a href="https://www.linkedin.com/in/tammy-husnetdinova-25150b171/">
-                        <i class="fab fa-2x fa-linkedin"></i>
-                    </a>
-                </li>
-                <li class="socials-item">
-                    <a href="https://www.youtube.com/channel/UCwiQYSftmZ2joZsotbUarTg">
-                        <i class="fab fa-2x fa-youtube"></i>
-                    </a>
-                </li>
-                </ul>
             </div>
     )
 }
